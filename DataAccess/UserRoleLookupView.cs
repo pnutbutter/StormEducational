83ae5ccb3ee386f4a60bac12bd3e833f974a1a67
pnutbutter <andrew_swallows@yahoo.com>
2016-01-23 16:37:02 -0700

    Custom Role Provider added

---
+++ DataAccess/UserRoleLookupView.cs
@@ -0,0 +1,41 @@
+﻿namespace DataAccess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("UserRoleLookupView")]
+    public partial class UserRoleLookupView
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UserId { get; set; }
+
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+ 
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+
+        [StringLength(128)]
+        public string AspNetUserId { get; set; }
+
+        [StringLength(256)]
+        public string Email { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UserRoleId { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RoleId { get; set; }
+
+        [StringLength(50)]
+        public string RoleName { get; set; }
+    }
+}
+
--- Website/Models/IdentityModels.cs
+++ Website/Models/IdentityModels.cs
@@ -3,6 +3,11 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Principal;
+using DataAccess;
+using System.Web.Security;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Website.Models
 {
@@ -28,6 +33,84 @@ namespace Website.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+    }
+
+    public class CustomRoleProvider : RoleProvider
+    {
+        private DatabaseContext db = new DatabaseContext();
+
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            string[] roles;
+            List<UserRoleLookupView> userRoles = db.UserRoleLookupViews.Where(r => r.Email == username).ToList();
+            if (userRoles.Count > 0)
+                roles = userRoles.Select(x => x.RoleName).ToArray();
+            else
+                roles = new string[0];
+            return roles;                
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            string[] roles = GetRolesForUser(username);
+            if (roles.Length > 0)
+                return roles.Contains(roleName);
+            else
+                return false;
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
