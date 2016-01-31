5303000d5f601adb51a0631b8774954b377c7222
pnutbutter <andrew_swallows@yahoo.com>
2016-01-31 13:39:00 -0700

    Beginning to teacher area for assignments

---
+++ Website/Areas/Teacher/Models/TeacherAssignmentsData/TeacherAssignmentsIndex.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Areas.Teacher.Models.TeacherAssignmentsData
+{
+    public class TeacherAssignmentsIndex
+    {
+        public List<Assignment> ItemList { get; set; }
+    }
+}
