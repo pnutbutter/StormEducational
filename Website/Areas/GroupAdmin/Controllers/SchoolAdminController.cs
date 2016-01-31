263c2f0c6192bbddf4349a3915949bad7575d61a
PukeyBoy9999 <Gary03082000@gmail.com>
2016-01-31 14:54:12 -0700

    Added code to loop through list of emails for school admins

--- Website/Areas/GroupAdmin/Controllers/SchoolAdminController.cs
+++ Website/Areas/GroupAdmin/Controllers/SchoolAdminController.cs
@@ -7,6 +7,25 @@ using System.Web;
 using System.Web.Mvc;
 using Website.Areas.GroupAdmin.Models.SchoolAdminData;
 
+using System.Net.Http;
+using System.Net.Mail;
+using SendGrid;
+using System.Net;
+using System.Configuration;
+
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Website.Models;
+using System.Diagnostics;
+
+
 namespace Website.Areas.GroupAdmin.Controllers
 {
     public class SchoolAdminController : Controller
@@ -84,6 +103,82 @@ namespace Website.Areas.GroupAdmin.Controllers
             return RedirectToAction("Index", new { Message = "Saved User as School Administrator", id = data.GroupId });
         }
 
+        [HttpGet]
+        [Authorize]
+        public ActionResult Email(int GroupId)
+        {
+            SchoolAdminEmail data = new SchoolAdminEmail();
+            data.GroupId = GroupId;
+            return View(data);
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Email(SchoolAdminEmail data)
+        {
+            
+            
+            try
+            {
+                if (!this.ModelState.IsValid)
+                {
+                    return View(data);
+                }
+
+
+                //parse email list
+                string[] emailArray = data.EmailList.Split(',');
+
+                for (int i = 0; i < emailArray.Length; i++)
+                {
+                    //Validate email?
+
+
+
+                    string Gary = emailArray[i];
+                    
+                    //Send Email
+                    var myMessage = new SendGridMessage();
+                    myMessage.AddTo(emailArray[i]);
+                    myMessage.From = new System.Net.Mail.MailAddress("Gary03082000@gmail.com", "Gary L.");
+                    myMessage.Subject = "New Admin Request";
+                    myMessage.Text = "Has requested you as an administrator ...  Please click on the link below to access the application.";
+                    myMessage.Html = "www.google.com";
+
+                    var credentials = new NetworkCredential(
+                               ConfigurationManager.AppSettings["mailAccount"],
+                               ConfigurationManager.AppSettings["mailPassword"]
+                               );
+
+                    // Create a Web transport for sending email.
+                    var transportWeb = new Web(credentials);
+
+                    // Send the email.
+                    if (transportWeb != null)
+                    {
+                        //await transportWeb.DeliverAsync(myMessage);
+                    }
+                    else
+                    {
+                        //Trace.TraceError("Failed to create Web transport.");
+                        //await Task.FromResult(0);
+                    }
+                }
+
+
+                
+
+                
+                //db.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return RedirectToAction("Index", new { Message = "Email(s) have been sent.", id = data.GroupId });
+        }
 
         [HttpGet]
         [Authorize]
--- Website/Areas/GroupAdmin/Models/SchoolAdminData/SchoolAdminEmail.cs
+++ Website/Areas/GroupAdmin/Models/SchoolAdminData/SchoolAdminEmail.cs
@@ -3,13 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Website.Areas.GroupAdmin.Models.SchoolAdminData
 {
     public class SchoolAdminEmail
     {
+        public int GroupId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int RoleId { get; set; }
+        
         public string Message { get; set; }
 
+        [Display(Name = "Recipient Email Address(s) (Seperate Multiple Emails by Commas")]
         public string EmailList { get; set; }
     }
 }
