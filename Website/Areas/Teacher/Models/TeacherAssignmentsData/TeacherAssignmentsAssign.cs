910093528acb8d571fcc889cfded5099e6ec7413
pnutbutter <andrew_swallows@yahoo.com>
2016-05-01 09:41:22 -0600

    Added assignment model and task txt files to remember where we left off

---
+++ Website/Areas/Teacher/Models/TeacherAssignmentsData/TeacherAssignmentsAssign.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Areas.Teacher.Models.TeacherAssignmentsData
+{
+    public class TeacherAssignmentsAssign
+    {
+        public int AssignmentId { get; set; }
+
+        public List<UserView> Students { get; set; }
+
+        public int[] SelectedStudents { get; set; }
+
+        public DateTime[] DueDates { get; set; }
+    }
+}
