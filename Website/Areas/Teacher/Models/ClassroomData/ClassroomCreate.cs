6070be5ec88c389c3bc17c30a8f9bf99b6e68cc1
pnutbutter <andrew_swallows@yahoo.com>
2016-05-01 10:32:34 -0600

    Classroom create model

---
+++ Website/Areas/Teacher/Models/ClassroomData/ClassroomCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Website.Areas.Teacher.Models.ClassroomData
+{
+    public class ClassroomCreate
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string GroupName { get; set; }
+    }
+}
