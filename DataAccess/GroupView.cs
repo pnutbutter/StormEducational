cc52871c8337646d5991daca7826aa034010ed91
pnutbutter <andrew_swallows@yahoo.com>
2016-06-18 09:27:22 -0600

    Fix to the group view causing null exception

--- DataAccess/GroupView.cs
+++ DataAccess/GroupView.cs
@@ -14,28 +14,19 @@
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int GroupId { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int GroupTypeId { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int? ParentGroupId { get; set; }
 
-        [Key]
-        [Column(Order = 3)]
         [StringLength(50)]
         public string GroupName { get; set; }
 
-        [Column(Order = 4)]
         public bool IsActive { get; set; }
 
-        [Column(Order = 5)]
         public int? OwnerUserId { get; set; }
 
-        [Key]
-        [Column(Order = 6)]
         [StringLength(50)]
         public string GroupTypeName { get; set; }
 
